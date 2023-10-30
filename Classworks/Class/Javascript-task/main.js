6eda5412c300470cfeef144cd65fa4fff9a1af08
SenuberR <senuberpenahli24@gmail.com>
2023-10-30 19:46:46 +0400

    lab task 7

---
+++ Classworks/Class/Javascript-task/main.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+// for( let i=1; i<=100; i++){
+
+// if(){
+//     console.log("true",  i)
+// }
+
+// }
+
+
+function loop ( a ){
+
+    for( let i=1; i<=a; i++){
+
+        if(i%2==1){
+            console.log(i)
+        }
+    } 
+}
+
+console.log(loop(10))
---
+++ Classworks/Lab/Task-7/main.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+// let country = prompt("case");
+
+// let capital = ""
+// switch(country){
+//     case "Azerbaijan":
+//         capital="Baku";
+//     break;
+
+//     case "Germany":
+//         capital="Berlin";
+//     break;
+
+//     case "Turkey":
+//         capital="Ankara";
+//     break;
+
+//     case "Georgia":
+//         capital="Tbilisi";
+//     break;
+
+//     default:
+//         capital="Not Country!"
+// }
+
+// alert(capital)
+
+
+
+
+
+
+let getSecond = prompt ("enter a second");
+
+    let minutes = getSeconds / 60;
+    let scnd = getSeconds % 60;
+    alert  (minutes + "minute" + " " + scnd + "second") ;
+
+    console.log(getSeconds)
+    
+
+
+
+
+
+
+
+
+
+// function factorial(n) {
+    
+//     let num=n-1
+  
+//     return n *  factorial(num);
+ 
+//  }
+  
+ 
+// console.log(factorial(4));
+
+
+         
