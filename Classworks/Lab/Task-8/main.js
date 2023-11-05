0af8e45e547979e159a047edf6ea58b01edb9462
SenuberR <senuberpenahli24@gmail.com>
2023-11-05 21:57:47 +0400

    fucntion task

---
+++ Classworks/Lab/Task-8/main.js
@@ -0,0 +1,94 @@
+"use strict"
+
+
+// let x=["salam"];
+// let size=x.length-1;
+// function getReverse(word){
+//     let result=""
+
+//     for(let i=0; i<x.length-1; i--){
+//         result= result+word[i];
+//     }
+
+// }
+
+
+// getReverse(x)
+// console.log(getReverse(x))
+
+
+
+
+// let x = "never give up";
+// let a=[];
+
+// function getLetter(x){
+//     let repeat=0
+
+//     for (let i = 0; i < x.length; i++){
+
+//         if(x[i]=='v'){
+           
+//             repeat++;
+
+//         };
+//     }
+//     return repeat
+// }
+
+// console.log(getLetter(x))
+
+const cars = [
+    {
+        color: "red",
+        type: "mercedes",
+        capacity: 5,
+        price:100
+    },
+
+    {
+        color: "blue",
+        type: "bmw",
+        capacity: 2,
+        price:150
+    },
+    {
+        color: "blue",
+        type: "ford",
+        capacity: 6,
+        price:200
+    },
+    {
+        color: "blue",
+        type: "bmw",
+        capacity: 6,
+        price:400
+    },
+]
+
+// const newArray = cars.map((car)=>{
+//     if (car.capacity <= 3) {
+//         return `small ${car.type}` 
+//       }
+//      if(car.capacity<=5){
+//         return `medium ${car.type}` 
+//       }
+//       else{
+//         return `large ${car.type}` 
+//       }
+// });
+
+// console.log(newArray)
+
+// const array=[  color: "red",type: "mercedes",capacity: 5,price:100]
+
+// const carPrice = cars.filter((car)=>car.color="blue").map((car)=>){
+//     let discount = car.price - (car.price*30)/100;
+// }
+
+// console.log(carPrice)
+
+
+//Tam bitməyib, yeniden yazilacaq!!!
+
+
