e377a63329e577ee28592ec61029baafa8eb5b3e
SenuberR <senuberpenahli24@gmail.com>
2023-11-29 14:17:39 +0400

    add js tasks

---
+++ Classworks/Class/Js-event-task/main.js
@@ -0,0 +1,16 @@
+
+
+
+// const btn=document.querySelector('.btn')
+// const side=document.querySelector('.side')
+// const box=document.querySelector('.box')
+// const icon=document.querySelector('.box_icon')
+
+// btn.addEventListener('click',function(){
+// side.classList.add("box")
+// })
+
+// icon.addEventListener('click',function(){
+// box.classList.remove("side")
+// })
+
--- Homeworks/Javascript-tasks/Js-task-2/main.js
+++ Homeworks/Javascript-tasks/Js-task-2/main.js
@@ -1,2 +1,15 @@
 "use strict"
 
+
+
+
+// Task 1: Function Basics
+
+//1
+let userName;
+
+function greetUser(name){
+    return 'hello'+' '+name
+}
+
+prompt(greetUser())
