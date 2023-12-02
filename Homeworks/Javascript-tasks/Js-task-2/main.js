1da38d9fb64b30e9441e5d41b4e9aaf85fae29cd
SenuberR <senuberpenahli24@gmail.com>
2023-12-02 12:14:10 +0400

    add task -2

--- Homeworks/Javascript-tasks/Js-task-2/main.js
+++ Homeworks/Javascript-tasks/Js-task-2/main.js
@@ -6,10 +6,75 @@
 // Task 1: Function Basics
 
 //1
-let userName;
 
-function greetUser(name){
-    return 'hello'+' '+name
-}
+// function greetUser(name){
+//    return prompt("hello")
+// }
+// console.log(greetUser())
 
-prompt(greetUser())
+
+//2
+
+// function calculateArea(a,b){
+//     if(a===b){
+//         return `Reactangle --- ${a**2}`
+//     }
+//     else{
+//         return `Shape --- ${a*b/2}`
+//     }
+// }
+// console.log(calculateArea(3,4))
+
+
+//3
+
+// function isEven(a){
+//     if(a%2===0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(isEven(5))
+
+
+
+//Task 2: Array Manipulation
+
+//1
+
+// let fruits = ['Strawberry', 'Mango','Avocado','Raspberry','Orange']
+
+// function displayFruits(arr){
+//     return  arr.map((item)=>`${item}-${item.length}`)
+// }
+// console.log( displayFruits(fruits))
+
+
+//2
+
+// let arrNumber=[1,2,3,4,5,6,7,8,9]
+// let arrString=['hello','new year','open','holiday']
+
+// function reverseArray(arr){
+// return arr.reverse()
+// }
+// console.log(reverseArray(arrNumber))
+// console.log(reverseArray(arrString))
+
+
+//3
+
+// let array=[1,2,3,4,5,6,7,8,9,10,15,24,39,60]
+// function filterEvenNumbers(arr){
+// return arr.filter((item)=>item%2==0)
+// }
+// console.log(filterEvenNumbers(array))
+
+
+
+//Task 3: Object and Methods
+
+//1
+
