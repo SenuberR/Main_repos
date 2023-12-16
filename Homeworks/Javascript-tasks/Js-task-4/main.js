82dfdaeb0f41d2d57a5e008be304384621b37692
SenuberR <senuberpenahli24@gmail.com>
2023-12-16 06:47:35 +0400

    add task 4

--- Homeworks/Javascript-tasks/Js-task-4/main.js
+++ Homeworks/Javascript-tasks/Js-task-4/main.js
@@ -11,6 +11,7 @@
 // console.log(getReverse('Hello world'))
 
 
+
 //2-- Repeat every character
 
 // function repeatEveryCharacters(str, n) {
@@ -39,12 +40,163 @@
 //   console.log( repeatCharacter('Hello world', 3))
 
 
+
 //4-- Not repeated character------??????
 
-// function notRepeated(){
+//5-Find the first repeated----????
+
+//6-- Count each character----????
+
+//7--Cut string----????
+
+//8--Cut string2----????
+
+
+
+
+//9--Get Sum numbers
+
+// let array= [1, 2, 3, 4, 5]
+// function getSumArray(arr){
+//     let sum=0
+//     for (let i = 0; i < arr.length; i++) {
+//         sum+=arr[i]
+//    }
+//     return sum
+// }
+// console.log(getSumArray(array))
+
+
+
+//10--Get Average
+
+// let array= [1, 2, 3, 4, 5]
+// function getSumArray(arr){
+//     let sum=0
+//     for (let i = 0; i < arr.length; i++) {
+//         sum+=arr[i]
+//    }
+//    let average=sum/arr.length
+//     return average
+// }
+// console.log(getSumArray(array))
+
+
+
+//11--Find maximum
+
+// let array= [1, 2, 3, 4, 5]
+
+// function getMax(arr) {
+//     let max = arr[0]
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] > max) {
+//         max = arr[i];
+//       }
+//     }
+  
+//     return max;
+//   }
+// console.log(getMax(array))
+
+
+
+//12--Find minimum
+
+// let array= [1, 2, 3, 4, 5]
+
+// function getMin(arr) {
+//     let min = arr[0]
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] < min) {
+//         min = arr[i];
+//       }
+//     }
+  
+//     return min;
+//   }
+// console.log(getMin(array))
+
+
+
+//13--Find second largest
+
+// let array= [1, 2, 3, 4, 5,6,7]
+// function getSecondLargest(arr) {
+//     let max = arr[0]
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] > max) {
+//         max = arr[i]-1;
+//       }
+//     }
+  
+//     return max;
+//   }
+// console.log(getSecondLargest(array))
+
+
+
+//14--Find second smallest
+
+// let array= [-1,0,1, 2, 3, 4, 5,6,7]
+// function getSecondSmallest(arr) {
+//     let min = arr[0]
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] < min) {
+//         min = arr[i]+1;
+//       }
+//     }
+  
+//     return min;
+//   }
+// console.log(getSecondSmallest(array))
+
+
+
+//15--Count Each element---?????
+
+// let array= [1, 2, 3, 4, 5, 1, 2, 3, 4, 5] 
+// function countElement(){
 
 // }
 
 
-//5
+
+//16 --Get odd sum
+
+// let array= [1, 2, 3, 4, 5] 
+
+// function getOddSum(item){
+//     let sum=0
+//     for (let i = 0; i < item.length; i++) {
+//     if(item[i]%2!==0){
+//         sum+=item[i]
+//     }
+//    }
+//     return sum
+// }
+
+// console.log(getOddSum(array))
+
+
+
+//17--Get even sum
+
+// let array= [1, 2, 3, 4, 5] 
+
+// function getEvenSum(item){
+//     let sum=0
+//     for (let i = 0; i < item.length; i++) {
+//     if(item[i]%2==0){
+//         sum+=item[i]
+//     }
+//    }
+//     return sum
+// }
+
+// console.log(getEvenSum(array))
 
